658b376820a52ab19646afccfb4393c7fd0c5166
Babar B <BobBaig@outlook.com>
2025-10-06 02:13:15 -0400

    Update is_armstrong_number.py
    Improve docstring

--- is_armstrong_number.py
+++ is_armstrong_number.py
@@ -4,7 +4,7 @@ import sys
 
 def is_armstrong_number(number: int) -> bool:
     """
-    An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+    An Armstrong number is the sum of its own digits each raised to the power of the number of digits.
     For example:
     9 is an Armstrong number, because 9 = 9^1 = 9
     10 is not an Armstrong number, because 10 != 1^2 + 0^2 = 1
