828e138597bdd51701cf4a6994b569e58ed681df
Babar B <BobBaig@outlook.com>
2025-10-06 05:36:49 +0000

    Code clean-up

--- is_armstrong_number.py
+++ is_armstrong_number.py
@@ -28,7 +28,7 @@ def main() -> int:
     except ValueError:
         print(f'Argument [{sys.argv[1]}] must be an integer.', file=sys.stderr)
         return 2
-    print(is_armstrong_number(int(sys.argv[1])))
+    print(is_armstrong_number(number))
     return 0
 
 
