3a243996490074df243ef9daf8c38d707508947b
Babar-B <BobBaig@outlook.com>
2025-10-06 23:25:42 -0400

    Switch from unittest to pytest

--- test_is_armstrong_number.py
+++ test_is_armstrong_number.py
@@ -1,24 +1,18 @@
-# test_is_armstrong_number.py
-import unittest
+# Unit tests for is_armstrong_number function
+# Call syntax1: python -m unittest test_is_armstrong_number.py
+# Call syntax2: pytest -v
+
+import pytest
 from is_armstrong_number import is_armstrong_number
 
-class TestArmstrongNumber(unittest.TestCase):
+def test_known_armstrong_numbers():
+    for n in [153, 370, 371, 407]:
+        assert is_armstrong_number(n)
 
-    def test_known_armstrong_numbers(self):
-        self.assertTrue(is_armstrong_number(153))
-        self.assertTrue(is_armstrong_number(370))
-        self.assertTrue(is_armstrong_number(371))
-        self.assertTrue(is_armstrong_number(407))
+def test_non_armstrong_numbers():
+    for n in [100, 200, 123]:
+        assert not is_armstrong_number(n)
 
-    def test_non_armstrong_numbers(self):
-        self.assertFalse(is_armstrong_number(100))
-        self.assertFalse(is_armstrong_number(200))
-        self.assertFalse(is_armstrong_number(123))
-
-    def test_single_digit_numbers(self):
-        for i in range(10):
-            with self.subTest(i=i):
-                self.assertTrue(is_armstrong_number(i))
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("n", range(10))
+def test_single_digit_numbers(n):
+    assert is_armstrong_number(n)
